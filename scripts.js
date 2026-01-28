b35be1f63ff4c4aa3a7b921db398100820c13f66
John Halvorson <johnmarch.halvorson@gmail.com>
2026-01-27 23:37:54 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,21 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const tabTitles = {
+  about: "About",
+  writing: "Writing",
+  resources: "Resources"
+};
 
 tabs.forEach(tab => {
   tab.addEventListener("click", () => {
     const target = tab.dataset.tab;
 
-    // deactivate all tabs
     tabs.forEach(t => t.classList.remove("active"));
-
-    // hide all panels
     panels.forEach(p => p.classList.remove("active"));
 
-    // activate clicked tab
     tab.classList.add("active");
+    document.querySelector(`.panel[data-panel="${target}"]`).classList.add("active");
 
-    // show matching panel
-    document
-      .querySelector(`.panel[data-panel="${target}"]`)
-      .classList.add("active");
+    document.title = `John Halvorson ⋅ ${tabTitles[target]}`;
   });
 });
